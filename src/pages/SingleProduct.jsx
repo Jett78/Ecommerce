cb971b3360cfead5f0e3bad87862408b14ee275c
Jeet <jetdeula@gmail.com>
2024-04-01 21:25:33 +0545

    updated

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { add } from "../redux/cartSlice";
 import { useDispatch } from "react-redux";
+import { FaStar } from "react-icons/fa6";
+import { FaRegStar } from "react-icons/fa6";
 
 const SingleProduct = () => {
   const dispatch = useDispatch();
@@ -23,82 +25,55 @@ const SingleProduct = () => {
   }, []);
   return (
     <main>
-      <div>
-        <section className="text-gray-600 body-font overflow-hidden">
-          <div className=" px-5 py-24 mx-auto">
-            <div className="lg:w-4/5 mx-auto flex flex-wrap">
-              <img
-                alt="ecommerce"
-                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                src={image}
-              />
-              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 bg-white">
-                <h1 className="text-gray-900 text-3xl title-font font-bold mb-10">
-                  {title}
-                </h1>
-                <div className="flex mb-4">
-                  <span className="flex items-center">
-                    <span className="text-gray-600 ml-3">4 Reviews</span>
-                  </span>
-                </div>
-                <p className=" text-xl"> {description}</p>
-                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                  <div className="flex">
-                    <span className="mr-3">Color</span>
-                    <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                    <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                    <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                  </div>
-                  <div className="flex ml-6 items-center">
-                    <span className="mr-3">Size</span>
-                    <div className="relative">
-                      <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                        <option>SM</option>
-                        <option>M</option>
-                        <option>L</option>
-                        <option>XL</option>
-                      </select>
-                      <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          className="w-4 h-4"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M6 9l6 6 6-6"></path>
-                        </svg>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex">
-                  <span className="title-font font-bold text-3xl text-gray-900">
-                    ${price}
-                  </span>
-                  <button
-                    className="bg-purple-500 text-white border-none outline-none w-40 p-1 my-20 "
-                    onClick={() => handleAdd(product)}
-                  >
-                    Add to cart
-                  </button>
-                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                    </svg>
-                  </button>
+      <h2 className="bg-white text-2xl font-bold p-4 ">Category: {category}</h2>
+      <div className=" bg-white p-16 flex flex-wrap sm:flex  justify-center gap-6 ">
+        <section>
+          <figure className="max-w-80 border-2 p-2">
+            <img src={image} alt="singleproduct" className="min-w-60 sm:w-full" />
+          </figure>
+        </section>
+
+        <section className="grid min-h-[30rem] items-center p-2">
+          <div>
+            <h2 className="text-4xl font-bold my-5 max-w-[35rem]">{title}</h2>
+            <div className="flex gap-2 text-yellow-400 text-xl">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaRegStar />
+            </div>
+            <p className="max-w-[35rem] font-semibold text-l text-gray-400 mt-2 ">
+              {description}
+            </p>
+            <section className="flex items-center gap-5 mt-2">
+              <div class="flex mt-5">
+                <span class="mr-3 font-semibold">Color</span>
+                <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+                <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
+              </div>
+
+              <div class="flex items-end">
+                <span class="mr-3 font-semibold">Size</span>
+                <div class="relative">
+                  <select class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-8">
+                    <option>SM</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                  </select>
                 </div>
               </div>
-            </div>
+
+            </section>
+            <h3 className="text-3xl font-bold mt-5">${price}</h3>
+            <button
+              onClick={() => handleAdd(product)}
+              className="bg-purple-500 text-white px-4 py-1 rounded-lg mt-5"
+            >
+              Add to Cart
+            </button>
           </div>
         </section>
       </div>
@@ -107,8 +82,3 @@ const SingleProduct = () => {
 };
 
 export default SingleProduct;
-
-
-
-
-
